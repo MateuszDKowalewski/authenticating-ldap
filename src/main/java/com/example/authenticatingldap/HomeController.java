ff23760f4e9e9e9bf1a1a615b07275a23723b702
mkowalewski <mateusz.kowalewski@nekken.pl>
2020-07-20 14:02:18 +0200

    Basic LDAP security

--- src/main/java/com/example/authenticatingldap/HomeController.java
+++ src/main/java/com/example/authenticatingldap/HomeController.java
@@ -6,19 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+//    @GetMapping("/")
+//    public String index() {
+//        return ("<h1>Welcome!</h1>");
+//    }
+//
+//    @GetMapping("/user")
+//    public String user() {
+//        return ("<h1>Welcome user!</h1>");
+//    }
+//
+//    @GetMapping("/admin")
+//    public String admin() {
+//        return ("<h1>Welcome admin!</h1>");
+//    }
+
     @GetMapping("/")
     public String index() {
-        return ("<h1>Welcome!</h1>");
-    }
-
-    @GetMapping("/user")
-    public String user() {
-        return ("<h1>Welcome user!</h1>");
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return ("<h1>Welcome admin!</h1>");
+        return "Welcome to the home page!";
     }
 
 }
--- src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
+++ src/main/java/com/example/authenticatingldap/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.authenticatingldap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,27 +8,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    DataSource dataSource;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "SELECT username, password, enabled " +
-                        "FROM users " +
-                        "WHERE username = ?"
-                ).authoritiesByUsernameQuery(
-                        "SELECT username, authority " +
-                        "FROM authorities " +
-                        "WHERE username = ?"
-                );
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=springframework,dc=org")
+                .and()
+                .passwordCompare()
+                .passwordAttribute("userPassword");
     }
 
     @Bean
@@ -37,27 +38,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/").permitAll()
-                .and().formLogin();
-    }
-
+//    @Autowired
+//    DataSource dataSource;
 //    @Override
 //    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .ldapAuthentication()
-//                .userDnPatterns("uid={0},ou=people")
-//                .groupSearchBase("ou=groups")
-//                .contextSource()
-//                .url("ldap://localhost:8389/dc=springframework,dc=org")
-//                .and()
-//                .passwordCompare()
-//                .passwordEncoder(new BCryptPasswordEncoder())
-//                .passwordAttribute("userPassword");
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery(
+//                        "SELECT username, password, enabled " +
+//                        "FROM users " +
+//                        "WHERE username = ?"
+//                ).authoritiesByUsernameQuery(
+//                        "SELECT username, authority " +
+//                        "FROM authorities " +
+//                        "WHERE username = ?"
+//                );
+//    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/admin").hasRole("ADMIN")
+//                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/").permitAll()
+//                .and().formLogin();
 //    }
 
 }
